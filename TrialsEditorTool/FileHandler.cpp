c78846efb94f06add9b6dd6d8823dd0116647280
kuhaufleitung <merk@hm.edu>
2021-01-26 20:09:40 +0100

    .

--- TrialsEditorTool/FileHandler.cpp
+++ TrialsEditorTool/FileHandler.cpp
@@ -14,13 +14,20 @@ void CFileHandle::mGetFirstFile(std::string input) {
 
 //retrieves Rest of TrackIDs
 void CFileHandle::mGetNextFiles() {
+	while (GetLastError() != ERROR_NO_MORE_FILES) {
 
+		if ((FileAttributes.dwFileAttributes | FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY || (*FileAttributes.cFileName == '.')) {
+			FindNextFileA(hFind, &FileAttributes);
+			mTrackID.push_back(FileAttributes.cFileName); //writes Path into a Array
+		}
+	}
+	mTrackID.erase(mTrackID.end() - 1, mTrackID.end()); //erases last double entry
+	FindClose(hFind);
 }
 
 
 //Starts all that shit
-void CFileHandle::Init() {
-	CPathHandle Path;
-	Path.mPathDefaults();
-	mGetFirstFile(Path.mGetEditorPath().c_str());
+void CFileHandle::Init(std::string classstring) {
+	mGetFirstFile(classstring);
+	mGetNextFiles();
 }
