88a660371750538b87756f5ec203ed3f612a726f
Hoax <topgun@fettabernett.de>
2021-01-28 18:35:24 +0100

    FetchTracknameArray

--- TrialsEditorTool/TrackArray.h
+++ TrialsEditorTool/TrackArray.h
@@ -2,17 +2,25 @@
 #include <Windows.h>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <iostream>
 
-
-class CFileHandle {
+class TrackArray {
 public:
 	void Init(std::string classstring);
 	void mGetFirstFile(std::string input);
 	void mGetNextFiles();
 
+	void mGetTracknameList(std::string Path);
+	//bool mNameFileFound(const char* filename);
+
 	std::vector<std::string> TrackID;
+	std::vector<std::string> TrackNames;
 
 private:
+	std::ifstream NameOpen;
+	std::string buffer;
+
 	HANDLE hFind;
 	WIN32_FIND_DATAA FileAttributes;
 };
