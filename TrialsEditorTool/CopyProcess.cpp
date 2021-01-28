dc5775999943a46b28b061e9efb24c14bd726c36
Hoax <topgun@fettabernett.de>
2021-01-28 17:37:44 +0100

    added templatepath

--- TrialsEditorTool/CopyProcess.cpp
+++ TrialsEditorTool/CopyProcess.cpp
@@ -1,2 +1,14 @@
 #include "CopyProcess.h"
 
+std::string CCopy::GetTemplatePath(std::string input) {
+
+	hFindTemp = FindFirstFileA((LPCSTR)(input + "//*").c_str(), &data);
+	TemplateFolder = data.cFileName;
+
+	while (TemplateFolder.find("0000000000000") == std::string::npos) {
+		FindNextFileA(hFindTemp, &data);
+		TemplateFolder = data.cFileName;
+	}
+	FindClose(hFindTemp);
+	return TemplateFolder;
+}
