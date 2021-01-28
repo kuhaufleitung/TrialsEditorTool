384cf1e7c1d90c049ce3077a8c8708a87a756ca1
kuhaufleitung <merk@hm.edu>
2021-01-28 17:37:44 +0100

    added templatepath

--- TrialsEditorTool/CopyProcess.h
+++ TrialsEditorTool/CopyProcess.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <fstream>
 #include <string>
-
+#include <Windows.h>
 
 class CCopy {
 public:
-	std::string GetTemplatePath();
+	std::string GetTemplatePath(std::string input);
+	std::string TemplateFolder;
+
+private:
+	std::fstream TemplateMetadata;
+	std::fstream TemplateDisplayname;
+	
+	HANDLE hFindTemp;
+	WIN32_FIND_DATAA data;
+
 };
