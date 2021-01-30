85664fdb8ba1945e2da0af2788dbb9c49de97021
Hoax <topgun@fettabernett.de>
2021-01-30 20:21:53 +0100

    create folder func

--- TrialsEditorTool/PortTrack.cpp
+++ TrialsEditorTool/PortTrack.cpp
@@ -2,7 +2,7 @@
 
 std::string CPort::mGetTemplatePath(std::string input, std::string ProfileID) {
 
-	hFindTemplate = FindFirstFileA((LPCSTR)(input + "//*").c_str(), &data);
+	hFindTemplate = FindFirstFileA((LPCSTR)(input + "\\*").c_str(), &data);
 	TemplateFolder = data.cFileName;
 
 	while (TemplateFolder.find(ProfileID) == std::string::npos) {
@@ -15,4 +15,9 @@ std::string CPort::mGetTemplatePath(std::string input, std::string ProfileID) {
 
 std::string CPort::mUseTemplatePath() {
 	return TemplateFolder;
+}
+
+//446ebf93f535b37276d4afbbbfd38a882642e85d02-0-0000000000000
+void CPort::mCreateFolder(std::string EditorPath, std::string ProfileID, std::string Unix) {
+	CreateDirectoryA((LPCSTR) (EditorPath + "\\" + ProfileID + Unix).c_str(), sec);
 }
--- TrialsEditorTool/PortTrack.h
+++ TrialsEditorTool/PortTrack.h
@@ -6,11 +6,13 @@ class CPort {
 public:
 	std::string mGetTemplatePath(std::string input, std::string ProfileID);
 	std::string mUseTemplatePath();
+	void mCreateFolder(std::string EditorPath, std::string ProfileID, std::string Unix);
 private:
 
 	std::string TemplateFolder;
 
+
 	HANDLE hFindTemplate;
 	WIN32_FIND_DATAA data;
-
+	LPSECURITY_ATTRIBUTES sec;
 };
