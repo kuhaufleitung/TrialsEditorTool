a478fa1d429b3ed2f9917e49c09a5f255958dbc3
Hoax <topgun@fettabernett.de>
2021-01-31 21:33:00 +0100

    bool for first window start to check cfg

--- TrialsEditorTool/PathHandler.cpp
+++ TrialsEditorTool/PathHandler.cpp
@@ -11,20 +11,27 @@ bool CPathHandle::mCfgFound(const char* filename)
 
 
 //opens cfg and writes StdPath if not created
-void CPathHandle::mPathDefaults() {
+bool CPathHandle::mPathDefaults() {
 
+	
 	if (mCfgFound("Path.cfg") == false) {
+
+		isConfigFileCreated = false;
+
 		CfgFile.open(PathName, std::fstream::out);
 		CfgFile << docdir << "\\Documents\\TrialsFusion\\SavedGames" << std::endl;
 		CfgFile << "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher" << std::endl << std::endl;
-		
+
 		CfgFile << "//FirstPath looks like this structure and points to your Trials SavedGames Directory:\n//C:\\Users\\*yourname*\\Documents\\TrialsFusion\\SavedGames" << std::endl << std::endl
 				<< "//SecondPath looks like this structure and points to your Ubisoft Game Launcher Directory, NOT your game:\n//C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher" << std::endl << std::endl
 				<< "//There have to be NO backslashes at the end!" << std::endl
 				<< "//This file gets auto generated when it isnt available in the program directory.";
-
+		
+		CfgFile.close();
 	}
-	CfgFile.close();
+	else { isConfigFileCreated = true; };
+	
+	return isConfigFileCreated;
 }
 
 
