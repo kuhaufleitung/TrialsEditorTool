27915214444b43b38948bbc02c17e01ad3798f24
Hoax <topgun@fettabernett.de>
2021-04-07 01:51:24 +0200

    included comctl32.lib for a better looking window.

--- TrialsEditorTool/framework.h
+++ TrialsEditorTool/framework.h
@@ -13,3 +13,8 @@
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+#include <CommCtrl.h>
+
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
